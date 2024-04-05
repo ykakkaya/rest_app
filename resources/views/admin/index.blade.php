9c4a567c0fd78c731a2df31336e804848977c8be
Yakup Akkaya <100940437+ykakkaya@users.noreply.github.com>
2024-04-05 15:11:07 +0300

    breeze & middleware add

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Burası admin sayfası</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Admin Panel Routes
+    Route::middleware('admin')->prefix('admin')->group(function () {
+        Route::get('/index', [AdminController::class, 'index'])->name('admin.index');
+    });
+
+});
+
+
+
+require __DIR__.'/auth.php';
